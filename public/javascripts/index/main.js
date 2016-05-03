0df0cb56576b1f7ddb6be75654d1293f136c053a
zxyuling <1275869735@qq.com>
2016-05-03 09:42:56 +0800

    纠正>>>错误

--- public/javascripts/index/main.js
+++ public/javascripts/index/main.js
@@ -1,12 +1,7 @@
 define(function(require)
 {
-<<<<<<< HEAD
 	var change=require('/widget/inup/change');
 	var hide=require('/widget/inup/hide');
 	change.change('.inup-link','.inup');
 	hide.hide('.inup-link');
-=======
-	var change=require('/widget/inup/change')
-	change.change('.inup-change','.inup');
->>>>>>> e4eef0ef456dbe73a12cbedd5188dd79c32cc44d
 });
